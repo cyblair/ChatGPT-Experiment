cb1abab698c0a37493f375e423328d4124d9fc99
cyblair <cy@theblairs.us>
2023-03-26 13:29:04 -0400

    Switched to ttk widgets, Added progress bar for completions

--- chatgpt.py
+++ chatgpt.py
@@ -1,7 +1,11 @@
 import os
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
 import openai
+import asyncio
+import threading
+import time
 
 global WINDOW_WIDTH
 global WINDOW_HEIGHT
@@ -10,6 +14,13 @@ WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 360
 MAX_TOKENS = 2048
 
+def asyncioThread(async_loop, collector):
+    async_loop.run_until_complete(collector.getCompletions(collector.getIterations()))
+
+def getCompletions(async_loop, collector):
+    threading.Thread(target=asyncioThread, args=(async_loop, collector)).start()
+
+# Gets a list of language models available
 def getModelIds():
     modelIdList = []
     modelList = openai.Model.list().data
@@ -19,47 +30,69 @@ def getModelIds():
 
 class ChatGPTCollector(tk.Frame):
     def __init__(self, master):
-        frtop = tk.Frame(master)
-        tk.Label(frtop, text="Folder to save projects to:").pack()
+        async_loop = asyncio.get_event_loop()
 
+        # Top Region
+        frtop = ttk.Frame(master)
+
+        # Save location input
+        ttk.Label(frtop, text="Folder to save projects to:").pack()
         self.folder = tk.StringVar()
-        tk.Entry(frtop, textvariable=self.folder, width=50).pack()
-        
-        tk.Button(frtop, text="Browse", command=self.browseFiles).pack()
+        ttk.Entry(frtop, textvariable=self.folder, width=50).pack()    
+        ttk.Button(frtop, text="Browse", command=self.browseFiles).pack()
+        # Pack top Region
         frtop.pack()
 
-        frleft = tk.Frame(master)
+        # Left Region
+        frleft = ttk.Frame(master)
 
-        tk.Label(frleft, text="Name of file:").pack()
+        # File name input
+        ttk.Label(frleft, text="Name of file:").pack()
         self.fileName = tk.StringVar()
-        tk.Entry(frleft, textvariable=self.fileName, width=10).pack()
+        ttk.Entry(frleft, textvariable=self.fileName, width=10).pack()
 
-        tk.Label(frleft, text="File Extension:").pack()
+        # File extension input
+        ttk.Label(frleft, text="File Extension:").pack()
         self.extension = tk.StringVar()
-        tk.Entry(frleft, textvariable=self.extension, width=10).pack()
+        ttk.Entry(frleft, textvariable=self.extension, width=10).pack()
 
-        tk.Label(frleft, text="Chat model to use:").pack()
-
+        # Chat Model selection
+        ttk.Label(frleft, text="Chat model to use:").pack()
         self.dropdown_modelType = tk.Listbox(frleft, selectmode=tk.SINGLE, height=6)
         chatModels = getModelIds()
         for model in chatModels:
             self.dropdown_modelType.insert(0, model)
         self.dropdown_modelType.pack()
+
+        #Pack Left Region
         frleft.pack(side=tk.LEFT)
 
-        frright = tk.Frame(master)
-        tk.Label(frright, text="Prompt:").pack()
+        # Right Region
+        frright = ttk.Frame(master)
+
+        # Prompt input
+        ttk.Label(frright, text="Prompt:").pack()
         self.prompt = tk.Text(frright, height=5, wrap=tk.WORD)
         self.prompt.pack()
-        tk.Label(frright, text="Iterations:").pack()
+        # Iterations input
+        ttk.Label(frright, text="Iterations:").pack()
         self.iterations = tk.StringVar()
-        tk.Entry(frright, textvariable=self.iterations).pack()
-        tk.Button(frright, text="Run", command=lambda:self.getCompletions(int(self.iterations.get()))).pack()
+        ttk.Entry(frright, textvariable=self.iterations).pack()
+        # Get OpenAI Completions
+        ttk.Button(frright, text="Run", command=lambda:getCompletions(async_loop, self)).pack()
+        
+        # Pack Right Region
         frright.pack(side=tk.RIGHT)
 
         master.title("ChatGPT Data Collector")
         master.geometry(str(WINDOW_WIDTH)+"x"+str(WINDOW_HEIGHT))
         master.mainloop()
+
+    def getIterations(self):
+        try:
+            return int(self.iterations.get())
+        except:
+            return 0
 
     def browseFiles(self):
         path = filedialog.askdirectory()
@@ -73,26 +106,48 @@ class ChatGPTCollector(tk.Frame):
         else:
             return ""
     
-    def getCompletions(self, iterations):
+    async def getCompletions(self, iterations):
+        win = tk.Tk()
+        prgrsLbl = ttk.Label(win, text="Progress (0/"+str(iterations)+"): ")
+        bar = ttk.Progressbar(win, maximum=iterations, value=0, length=250)
+        etaLbl = ttk.Label(win, text="Estimated Time remaining: ")
+        prgrsLbl.pack()
+        bar.pack()
+        etaLbl.pack()
+
+        win.geometry("300x80")
+
+        start = time.time()
         model = self.getSelectedChatModel()
+        print(openai.Model.retrieve(model))
         chatPrompt = self.prompt.get("1.0", tk.END)
         for i in range(iterations):
-            name = self.fileName.get()+str(i)
-            completion = openai.Completion.create(
-                model=model,
-                prompt=chatPrompt.format(title=name),
-                max_tokens=MAX_TOKENS
-            )
-            f = open(self.folder.get()+"/"+name+self.extension.get(), "w")
-            f.write(completion.choices[0].text)
-            f.close()
-        return
+            await self.getCompletion(model, chatPrompt, i)
+            prgrsLbl['text'] = "Progress ("+str(i)+"/"+str(iterations)+"): "
+            bar['value'] = i+1
+            eta = ((time.time()-start)/(i+1))*(iterations-i)
+            etaLbl['text'] = "Estimated time remaining: "+str(round(eta))+"s"
+            win.update()
+
+        win.destroy()
+    
+    async def getCompletion(self, model, chatPrompt, iteration):
+        i = iteration
+        name = self.fileName.get()+str(i)
+        completion = openai.Completion.create(
+            model=model,
+            prompt=chatPrompt.format(title=name),
+            max_tokens=MAX_TOKENS
+        )
+        f = open(self.folder.get()+"/"+name+self.extension.get(), "w")
+        f.write(completion.choices[0].text)
+        f.close()
 
 def main():
     key = os.getenv("OPENAI_API_KEY")
     openai.api_key = key
 
-    ChatGPTCollector(tk.Tk())
+    collector = ChatGPTCollector(tk.Tk())
     
 if __name__ == "__main__":
     main()
