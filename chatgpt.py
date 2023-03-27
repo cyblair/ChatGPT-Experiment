c63830ba60266fdef6fd52cfd34c9f9a7be00d32
cyblair <cy@theblairs.us>
2023-03-27 18:25:31 -0400

    Updated threading
    Progress window no longer tied down by being on the same thread as getCompletions but still gets updated by it.
    Also added more comprehensive comments

--- chatgpt.py
+++ chatgpt.py
@@ -12,11 +12,12 @@ import json
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 360
 MAX_TOKENS = 2048
-FILES = [("All Files", "*.*"),
-        ("JSON files", "*.json")]
+FILES = [("JSON files", "*.json"),
+         ("All Files", "*.*")
+        ]
 
 def asyncioThread(async_loop, collector):
-    async_loop.run_until_complete(collector.getCompletions(collector.getIterations()))
+    async_loop.run_until_complete(collector.getCompletions(collector.getIterations(), collector.getSelectedChatModel()))
 
 def getCompletions(async_loop, collector):
     threading.Thread(target=asyncioThread, args=(async_loop, collector)).start()
@@ -31,7 +32,7 @@ def getModelIds():
 
 class ChatGPTCollector(tk.Frame):
     def __init__(self, master):
-        async_loop = asyncio.get_event_loop()
+        self.async_loop = asyncio.get_event_loop()
 
         # Top Region
         frtop = ttk.Frame(master)
@@ -40,7 +41,7 @@ class ChatGPTCollector(tk.Frame):
         ttk.Label(frtop, text="Folder to save projects to:").pack()
         self.folder = tk.StringVar()
         ttk.Entry(frtop, textvariable=self.folder, width=50).pack()    
-        ttk.Button(frtop, text="Browse", command=self.browseFiles).pack()
+        ttk.Button(frtop, text="Browse", command=self.selectSaveFolder).pack()
         # Pack top Region
         frtop.pack()
 
@@ -80,7 +81,7 @@ class ChatGPTCollector(tk.Frame):
         self.iterations = tk.StringVar()
         ttk.Entry(frright, textvariable=self.iterations).pack()
         # Get OpenAI Completions
-        ttk.Button(frright, text="Run", command=lambda:getCompletions(async_loop, self)).pack()
+        ttk.Button(frright, text="Run", command=lambda:self.startCompletionProcess(self.getIterations())).pack()
 
         ttk.Label(frright, text="Configurations:").pack(side=tk.LEFT, padx=20)
         ttk.Button(frright, text="Save", command=self.saveConfig).pack(side=tk.LEFT)
@@ -89,32 +90,66 @@ class ChatGPTCollector(tk.Frame):
         # Pack Right Region
         frright.pack(side=tk.RIGHT)
 
+        # Set up master window
         master.title("ChatGPT Data Collector")
         master.geometry(str(WINDOW_WIDTH)+"x"+str(WINDOW_HEIGHT))
         master.mainloop()
 
+    def startCompletionProcess(self, iterations):
+        # Make a new window to display progress
+        self.win = tk.Tk()
+        self.prgrsLbl = ttk.Label(self.win, text="Progress (0/"+str(iterations)+"): ")
+        # For every iteration, we update value on getting completion and finishing writing to file
+        maximum = iterations*2
+        self.bar = ttk.Progressbar(self.win, maximum=maximum, value=0, length=250)
+        self.etaLbl = ttk.Label(self.win, text="Estimated Time remaining: ")
+        self.prgrsLbl.pack()
+        self.bar.pack()
+        self.etaLbl.pack()
+        self.win.geometry("300x80")
+
+        # Get our process start time
+        self.start = time.time()
+        # Start the process
+        getCompletions(self.async_loop, self)
+        # Call our progress window's main loop
+        self.win.mainloop()
+
+    """Save a .json file containing the data currently entered into fields"""
     def saveConfig(self):
+        # Get file
         f = filedialog.asksaveasfile(confirmoverwrite=True, filetypes=FILES, defaultextension=FILES)
+        # Create json string from object
         jstring = json.dumps({'folder':self.folder.get(), 'file':self.fileName.get(), 
                               'ext':self.extension.get(), 'model':self.dropdown_modelType.curselection(),
-                              'prompt':self.prompt.get("1.0", tk.END)})
+                              'prompt':self.prompt.get("1.0", tk.END)
+                              })
+        # Write json to file and close
         f.write(jstring)
         f.close()
 
+    """Load a .json file and write it's data to this windows fields"""
     def loadConfig(self):
+        # Get file
         f = filedialog.askopenfile(filetypes=FILES, defaultextension=FILES)
+        
+        # If no file was selected, return
         if (f == None):
             return
         
+        # Try to read data and write to fields
         try:
             jstring = f.read()
             obj = json.loads(jstring)
 
+            # Load up vars. If we don't do this, and a variable is missing, config will be partly loaded.
+            # We just want to throw out malformatted json files without changing anything
             folder = obj['folder']
             file = obj['file']
             ext = obj['ext']
             model = obj['model']
             prompt = obj['prompt']
+
             self.folder.set(folder)
             self.fileName.set(file)
             self.extension.set(ext)
@@ -122,24 +157,31 @@ class ChatGPTCollector(tk.Frame):
                 self.dropdown_modelType.see(model[0])
                 self.dropdown_modelType.index(model[0])
                 self.dropdown_modelType.activate(model[0])
+            # Clear current prompt and then insert prompt from file
             self.prompt.delete("1.0", tk.END)
             self.prompt.insert("1.0", prompt)
+        # If something fails, i.e. malformatted file, wrong file type, etc., show an error
         except:
             messagebox.showerror(title="File Error", 
                                 message="Something went wrong trying to load the specified file."+
                                 " Please only select files saved with this program.")
+        # Close file
         f.close()
 
+    """Gets the int entered in the iterations field"""
     def getIterations(self):
         try:
             return int(self.iterations.get())
+        # If the field is empty, default to 0
         except:
             return 0
 
-    def browseFiles(self):
+    """Allows the user to set which folder to save outputs to"""
+    def selectSaveFolder(self):
         path = filedialog.askdirectory()
         self.folder.set(path)
 
+    """Gets the String name of the selected language model"""
     def getSelectedChatModel(self):
         menu = self.dropdown_modelType
         selection = menu.curselection()
@@ -148,44 +190,45 @@ class ChatGPTCollector(tk.Frame):
         else:
             return ""
     
-    async def getCompletions(self, iterations):
-        win = tk.Tk()
-        prgrsLbl = ttk.Label(win, text="Progress (0/"+str(iterations)+"): ")
-        bar = ttk.Progressbar(win, maximum=iterations, value=0, length=250)
-        etaLbl = ttk.Label(win, text="Estimated Time remaining: ")
-        prgrsLbl.pack()
-        bar.pack()
-        etaLbl.pack()
-
-        win.geometry("300x80")
-        win.update()
-
-        start = time.time()
-        model = self.getSelectedChatModel()
+    """Starts the process of submitting prompts and getting completions from OpenAI"""
+    async def getCompletions(self, iterations, model):
+        # Set up vars
         chatPrompt = self.prompt.get("1.0", tk.END)
+        # Do process for each iteration
         for i in range(iterations):
+            # Get the completion
             await self.getCompletion(model, chatPrompt, i)
-            prgrsLbl['text'] = "Progress ("+str(i+1)+"/"+str(iterations)+"): "
-            bar['value'] = i+1
-            etaSeconds = round(((time.time()-start)/(i+1))*(iterations-i))
+            # Update progress
+            self.prgrsLbl['text'] = "Progress ("+str(i+1)+"/"+str(iterations)+"): "
+            # Estimate time remaining
+            etaSeconds = round(((time.time()-self.start)/(i+1))*(iterations-i))
             etaMinutes, etaSeconds = divmod(etaSeconds, 60)
-            etaLbl['text'] = "Estimated time remaining: %dm %ds" % (etaMinutes, etaSeconds)
-            win.update()
-
-        win.destroy()
+            # Update time estimation
+            self.etaLbl['text'] = "Estimated time remaining: %dm %ds" % (etaMinutes, etaSeconds)
+        
+        # Once we are done getting completions, close the progress window
+        self.win.destroy()
+        # Show the user a message to indicate the process completed successfully
+        messagebox.showinfo(title="Complete", message="Saved %d responses with the %s language model." % (iterations, model))
     
+    """Make request to the respective OpenAI model and save completion"""
     async def getCompletion(self, model, chatPrompt, iteration):
         i = iteration
         name = self.fileName.get()+str(i)
+        # TODO: Expand exception handling to modify request for different models
         try:
+            # Get a completion
             completion = openai.Completion.create(
                 model=model,
                 prompt=chatPrompt.format(title=name),
                 max_tokens=MAX_TOKENS
-            )
+                )
+            self.bar['value'] += 1
+            # Write completion to file
             f = open(self.folder.get()+"/"+name+self.extension.get(), "w")
             f.write(completion.choices[0].text)
             f.close()
+            self.bar['value'] += 1
         except openai.InvalidRequestError as e:
             print(e)
 
